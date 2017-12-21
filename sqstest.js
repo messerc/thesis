4a399347c57d7ee5f4b969ece98f46674dd82500
messerc <messer55@gmail.com>
2017-12-21 15:52:14 -0800

    modified queue test to accept real (but still fake) events data and post to DB

--- sqstest.js
+++ sqstest.js
@@ -1,11 +1,12 @@
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
+const moment = require("moment"); 
 // Set the region
 AWS.config.loadFromPath("./config.json");
 
 // Create an SQS service object
-var sqs = new AWS.SQS();
+const sqs = new AWS.SQS();
 
-var params = {
+const params = {
   QueueUrl: "https://sqs.us-east-2.amazonaws.com/669306556214/events"
 };
 
@@ -17,8 +18,11 @@ while (length) {
       let messages = [];
       for (let i = 0; i < 100; i++) {
         messages.push({
-          id: 1,
-          text: "This is a test message"
+          createdAt: moment('2016-01-01'),
+          eventType: 'view',
+          userId: 78,
+          listingId: 800,
+          experienceId: 900
         });
       }
       params.MessageBody = JSON.stringify(messages);
