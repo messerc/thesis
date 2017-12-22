b093053980700dbd3f7430c75291ce4faab0b4f0
messerc <messer55@gmail.com>
2017-12-22 12:28:25 -0800

    added integration tests for my post route

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const chai = require("chai");
 const { assert, expect, should } = chai;
 
@@ -17,27 +18,33 @@ describe("Events DB", function() {
       });
     });
   });
-  describe("#insert", function() {
+  describe("POST events", function() {
     after(function() {
-      Events.remove({where: {"eventType": "test"}}); 
-    })
-    it("should insert a record into the database", function() {
-      let testEvent = new Events({
-        createdAt: new Date("2016, 01, 01"),
-        eventType: "test",
-        userId: "test",
-        listingId: "test",
-        experienceId: "test"
-      });
-      testEvent.save().then(val => {
-        expect(val).to.exist;
+      Events.remove({ where: { eventType: "test" } });
+    });
+    it("should insert 100 records into the database at a time", function() {
+      let messages = [];
+      for (let i = 0; i < 100; i++) {
+        messages.push({
+          createdAt: new Date('2016', '01', '01'), 
+          eventType: 'test',
+          userId: 'test',
+          listingId: 'test',
+          experienceId: 'test'
+        });
+      }
+      axios.post('http://localhost:3000/events', {Body: JSON.stringify(messages)})
+      .then(val => expect(val).to.equal("finished writing 100 records"));
+    });
+    it("should retrieve written 100 records test events", function() {
+      Events.find({ where: { eventType: "test" } }).then(val => {
+        expect(val.length).to.equal(100);
       });
     });
-    it("should retrieve written record from database", function() {
-      Events.findOne({where: {"eventType": "test"}})
-      .then(val => {
-        expect(val.eventType).to.equal("test");
-      })
+    it("should retrieve test events", function() {
+      Events.find({ where: { eventType: "test" } }).then(val => {
+        expect(val[0].eventType).to.equal("test"); 
+      });
     });
   });
 });
